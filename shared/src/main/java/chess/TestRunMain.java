c097d524b9c3d9da994f2a8d8f65bbd5655c3266
Logan Brewer <lbrewer4@byu.edu>
2024-01-20 20:29:36 -0700

    Rook move calculator

--- shared/src/main/java/chess/TestRunMain.java
+++ shared/src/main/java/chess/TestRunMain.java
@@ -4,21 +4,25 @@ import java.util.ArrayList;
 import java.util.Collection;
 import chess.pieces.KnightMovesCalculator;
 import chess.pieces.BishopMovesCalculator;
+import chess.pieces.RookMovesCalculator;
+import chess.pieces.KingMovesCalculator;
+import chess.pieces.QueenMovesCalculator;
+import chess.pieces.PawnMovesCalculator;
 
 public class TestRunMain {
     public static void main(String[] args) {
         ChessBoard chessBoard = new ChessBoard();
         chessBoard.resetBoard();
 
-        chessBoard.addPiece(new ChessPosition(4, 4), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP));
+        chessBoard.addPiece(new ChessPosition(4, 4), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK));
         chessBoard.printBoard();
 
         // Testing KnightMovesCalculator
-        ChessPosition bishopPosition = new ChessPosition(4, 4); // Choose a position for the bishop
-        BishopMovesCalculator bishopCalculator = new BishopMovesCalculator();
-        Collection<ChessMove> bishopMoves = bishopCalculator.pieceMoves(chessBoard, bishopPosition);
+        ChessPosition piecePosition = new ChessPosition(4, 4); // Choose a position for the bishop
+        RookMovesCalculator calc = new RookMovesCalculator();
+        Collection<ChessMove> bishopMoves = calc.pieceMoves(chessBoard, piecePosition);
 
-        System.out.println("Valid moves for the bishop at position " + bishopPosition + ":");
+        System.out.println("Valid moves for the bishop at position " + piecePosition + ":");
         for (ChessMove move : bishopMoves) {
             System.out.println(move.getEndPosition());
         }
--- shared/src/main/java/chess/pieces/RookMovesCalculator.java
+++ shared/src/main/java/chess/pieces/RookMovesCalculator.java
@@ -2,6 +2,7 @@ package chess.pieces;
 
 import chess.ChessBoard;
 import chess.ChessMove;
+import chess.ChessPiece;
 import chess.ChessPosition;
 
 import java.util.ArrayList;
@@ -11,23 +12,77 @@ public class RookMovesCalculator {
 
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         Collection<ChessMove> validMoves = new ArrayList<>();
-        //TODO: Figure out logic for when a pieace is in the way
+
+        ChessPiece myPiece = board.getPiece(myPosition);
         int row = myPosition.getRow();
         int col = myPosition.getColumn();
 
-        for (int i = row + 1; i <= 8; i++) {
-            validMoves.add(new ChessMove(myPosition, new ChessPosition(i, col), null));
-        }
-        for (int i = row - 1; i >= 1; i--) {
-            validMoves.add(new ChessMove(myPosition, new ChessPosition(i, col), null));
+        for (int i = 1; i <= 8; i++) {
+            if (col + i <=8) {
+                ChessPosition newPosition = new ChessPosition(row, col + i);
+                ChessPiece targetPiece = board.getPiece(newPosition);
+                if (targetPiece != null && targetPiece.getTeamColor() == myPiece.getTeamColor()) {
+                    break;
+                }
+                if (targetPiece != null && targetPiece.getTeamColor() != myPiece.getTeamColor()) {
+                    validMoves.add(new ChessMove(myPosition, new ChessPosition(row, col + i), null));
+                    break;
+                }
+                else {
+                    validMoves.add(new ChessMove(myPosition, new ChessPosition(row, col + i), null));
+                }
+            }
         }
 
-        // Rook can move horizontally
-        for (int j = col + 1; j <= 8; j++) {
-            validMoves.add(new ChessMove(myPosition, new ChessPosition(row, j), null));
+        for (int i = 1; i <= 8; i++) {
+            if (col - i > 0) {
+                ChessPosition newPosition = new ChessPosition(row, col - i);
+                ChessPiece targetPiece = board.getPiece(newPosition);
+                if (targetPiece != null && targetPiece.getTeamColor() == myPiece.getTeamColor()) {
+                    break;
+                }
+                if (targetPiece != null && targetPiece.getTeamColor() != myPiece.getTeamColor()) {
+                    validMoves.add(new ChessMove(myPosition, new ChessPosition(row, col - i), null));
+                    break;
+                }
+                else {
+                    validMoves.add(new ChessMove(myPosition, new ChessPosition(row, col - i), null));
+                }
+            }
         }
-        for (int j = col - 1; j >= 1; j--) {
-            validMoves.add(new ChessMove(myPosition, new ChessPosition(row, j), null));
+
+        for (int i = 1; i <= 8; i++) {
+            if (row - i > 0) {
+                ChessPosition newPosition = new ChessPosition(row - i, col);
+                ChessPiece targetPiece = board.getPiece(newPosition);
+                if (targetPiece != null && targetPiece.getTeamColor() == myPiece.getTeamColor()) {
+                    break;
+                }
+                if (targetPiece != null && targetPiece.getTeamColor() != myPiece.getTeamColor()) {
+                    validMoves.add(new ChessMove(myPosition, new ChessPosition(row - i, col), null));
+                    break;
+                }
+                else {
+                    validMoves.add(new ChessMove(myPosition, new ChessPosition(row - i, col), null));
+                }
+            }
+        }
+
+        for (int i = 1; i <= 8; i++) {
+            if (row + i > 0) {
+                ChessPosition newPosition = new ChessPosition(row + i, col);
+                ChessPiece targetPiece = board.getPiece(newPosition);
+                if (targetPiece != null && targetPiece.getTeamColor() == myPiece.getTeamColor()) {
+                    break;
+                }
+                if (targetPiece != null && targetPiece.getTeamColor() != myPiece.getTeamColor()) {
+                    validMoves.add(new ChessMove(myPosition, new ChessPosition(row + i, col), null));
+                    break;
+                }
+                else {
+                    validMoves.add(new ChessMove(myPosition, new ChessPosition(row + i, col), null));
+                }
+            }
         }
 
         return validMoves;
