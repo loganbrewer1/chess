fefd87ab9eaac1b7b8e77b3cc8bb3cb386e0b098
Logan Brewer <lbrewer4@byu.edu>
2024-01-20 20:17:29 -0700

    Bishop move calculator

--- shared/src/main/java/chess/pieces/BishopMovesCalculator.java
+++ shared/src/main/java/chess/pieces/BishopMovesCalculator.java
@@ -2,6 +2,7 @@ package chess.pieces;
 
 import chess.ChessBoard;
 import chess.ChessMove;
+import chess.ChessPiece;
 import chess.ChessPosition;
 
 import java.util.ArrayList;
@@ -11,6 +12,78 @@ public class BishopMovesCalculator {
 
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         Collection<ChessMove> validMoves = new ArrayList<>();
+
+        ChessPiece myPiece = board.getPiece(myPosition);
+        int row = myPosition.getRow();
+        int col = myPosition.getColumn();
+
+        for (int i = 1; i <= 8; i++) {
+            if (row + i <= 8 && col + i <=8) {
+                ChessPosition newPosition = new ChessPosition(row + i, col + i);
+                ChessPiece targetPiece = board.getPiece(newPosition);
+                if (targetPiece != null && targetPiece.getTeamColor() == myPiece.getTeamColor()) {
+                    break;
+                }
+                if (targetPiece != null && targetPiece.getTeamColor() != myPiece.getTeamColor()) {
+                    validMoves.add(new ChessMove(myPosition, new ChessPosition(row + i, col + i), null));
+                    break;
+                }
+                else {
+                    validMoves.add(new ChessMove(myPosition, new ChessPosition(row + i, col + i), null));
+                }
+            }
+        }
+
+        for (int i = 1; i <= 8; i++) {
+            if (row + i <= 8 && col - i > 0) {
+                ChessPosition newPosition = new ChessPosition(row + i, col - i);
+                ChessPiece targetPiece = board.getPiece(newPosition);
+                if (targetPiece != null && targetPiece.getTeamColor() == myPiece.getTeamColor()) {
+                    break;
+                }
+                if (targetPiece != null && targetPiece.getTeamColor() != myPiece.getTeamColor()) {
+                    validMoves.add(new ChessMove(myPosition, new ChessPosition(row + i, col - i), null));
+                    break;
+                }
+                else {
+                    validMoves.add(new ChessMove(myPosition, new ChessPosition(row + i, col - i), null));
+                }
+            }
+        }
+
+        for (int i = 1; i <= 8; i++) {
+            if (row - i > 0 && col + i <=8) {
+                ChessPosition newPosition = new ChessPosition(row - i, col + i);
+                ChessPiece targetPiece = board.getPiece(newPosition);
+                if (targetPiece != null && targetPiece.getTeamColor() == myPiece.getTeamColor()) {
+                    break;
+                }
+                if (targetPiece != null && targetPiece.getTeamColor() != myPiece.getTeamColor()) {
+                    validMoves.add(new ChessMove(myPosition, new ChessPosition(row - i, col + i), null));
+                    break;
+                }
+                else {
+                    validMoves.add(new ChessMove(myPosition, new ChessPosition(row - i, col + i), null));
+                }
+            }
+        }
+
+        for (int i = 1; i <= 8; i++) {
+            if (row - i > 0 && col - i > 0) {
+                ChessPosition newPosition = new ChessPosition(row - i, col - i);
+                ChessPiece targetPiece = board.getPiece(newPosition);
+                if (targetPiece != null && targetPiece.getTeamColor() == myPiece.getTeamColor()) {
+                    break;
+                }
+                if (targetPiece != null && targetPiece.getTeamColor() != myPiece.getTeamColor()) {
+                    validMoves.add(new ChessMove(myPosition, new ChessPosition(row - i, col - i), null));
+                    break;
+                }
+                else {
+                    validMoves.add(new ChessMove(myPosition, new ChessPosition(row - i, col - i), null));
+                }
+            }
+        }
 
         return validMoves;
     }
