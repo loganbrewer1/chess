3ce29b215ed0bb4cf50ccf2ac0a11657bd130dce
Logan Brewer <lbrewer4@byu.edu>
2024-01-22 11:47:13 -0700

    Black piece promotions

--- shared/src/main/java/chess/pieces/PawnMovesCalculator.java
+++ shared/src/main/java/chess/pieces/PawnMovesCalculator.java
@@ -13,7 +13,6 @@ public class PawnMovesCalculator {
         ChessPiece myPiece = board.getPiece(myPosition);
         int row = myPosition.getRow();
         int col = myPosition.getColumn();
-        ChessPiece.PieceType thePromotion = null;
 
         if (myPiece.getTeamColor() == ChessGame.TeamColor.WHITE) {
             //If there isn't a piece 1 position in front of it... it can move there
@@ -55,21 +54,36 @@ public class PawnMovesCalculator {
         if (myPiece.getTeamColor() == ChessGame.TeamColor.BLACK) {
             //If there isn't a piece 1 position in front of it... it can move there
             if (board.getPiece(new ChessPosition(row - 1, col)) == null) {
-                validMoves.add(new ChessMove(myPosition, new ChessPosition(row - 1, col), null));
-                //If it's still in it's starting row it can move two assuming there isn't a piece there
-                if (row == 7) {
-                    if (board.getPiece(new ChessPosition(row - 2, col)) == null){
-                        validMoves.add(new ChessMove(myPosition, new ChessPosition(row - 2, col), null));
+                if (row != 2) {
+                    validMoves.add(new ChessMove(myPosition, new ChessPosition(row - 1, col), null));
+                    //If it's still in it's starting row it can move two assuming there isn't a piece there
+                    if (row == 7) {
+                        if (board.getPiece(new ChessPosition(row - 2, col)) == null){
+                            validMoves.add(new ChessMove(myPosition, new ChessPosition(row - 2, col), null));
+                        }
                     }
+                }
+                if (row == 2) {
+                    addAllPromotions(validMoves, myPosition,row - 1, col);
                 }
             }
             //If there are enemy pieces on a diagonal, those become valid moves.
-            if (board.getPiece(new ChessPosition( row - 1, col + 1)) != null || board.getPiece(new ChessPosition( row - 1, col - 1)) != null) {
-                if (board.getPiece(new ChessPosition( row - 1, col + 1)).getTeamColor() == ChessGame.TeamColor.BLACK) {
-                    validMoves.add(new ChessMove(myPosition, new ChessPosition(row - 1, col + 1), null));
+            if (board.getPiece(new ChessPosition( row - 1, col - 1)) != null || board.getPiece(new ChessPosition( row - 1, col + 1)) != null) {
+                if (board.getPiece(new ChessPosition( row - 1, col - 1)).getTeamColor() == ChessGame.TeamColor.WHITE) {
+                    if (row != 2) {
+                        validMoves.add(new ChessMove(myPosition, new ChessPosition(row - 1, col - 1), null));
+                    }
+                    if (row == 2) {
+                        addAllPromotions(validMoves, myPosition, row - 1, col - 1);
+                    }
                 }
-                if (board.getPiece(new ChessPosition( row - 1, col - 1)).getTeamColor() == ChessGame.TeamColor.BLACK) {
-                    validMoves.add(new ChessMove(myPosition, new ChessPosition(row - 1, col - 1), null));
+                if (board.getPiece(new ChessPosition( row - 1, col + 1)).getTeamColor() == ChessGame.TeamColor.WHITE) {
+                    if (row != 2) {
+                        validMoves.add(new ChessMove(myPosition, new ChessPosition(row - 1, col + 1), null));
+                    }
+                    if (row == 2) {
+                        addAllPromotions(validMoves, myPosition, row - 1, col + 1);
+                    }
                 }
             }
         }
