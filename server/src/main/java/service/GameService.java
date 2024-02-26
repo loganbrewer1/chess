004ac6cb8235b1d67f59b6c0a913e04620f347f5
Logan Brewer <lbrewer4@byu.edu>
2024-02-25 19:36:26 -0700

    ListGames for GameService

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,7 +1,33 @@
 package service;
 
+import dataAccess.AuthDAO;
+import dataAccess.DataAccessException;
+import dataAccess.GameDAO;
+import model.GameData;
+
+import java.util.Collection;
+import java.util.Map;
+
 public class GameService {
-    public void ListGames() {}
+
+    private final AuthDAO authDatabase;
+    private final GameDAO gameDatabase;
+
+    public GameService(AuthDAO authDatabase, GameDAO gameDatabase) {
+        this.authDatabase = authDatabase;
+        this.gameDatabase = gameDatabase;
+    }
+
+    public Map<Integer, GameData> ListGames(String authToken) {
+        try {
+            if (authDatabase.getAuth(authToken) == null) {
+                throw new RuntimeException("Not a valid authToken");
+            }
+            return gameDatabase.listGames();
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
     public void CreateGame() {}
     public void JoinGame() {}
 }
