a6c38c37d4526057f4daaa0afbd100b3529f4703
Logan Brewer <lbrewer4@byu.edu>
2024-02-25 20:11:08 -0700

    JoinGame in the GameService

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -9,6 +9,7 @@ import model.GameData;
 
 import java.util.Collection;
 import java.util.Map;
+import java.util.Objects;
 
 public class GameService {
 
@@ -43,7 +44,26 @@ public class GameService {
             throw new RuntimeException(e);
         }
     }
-    public void JoinGame() {}
+    public void JoinGame(String authToken, String playerColor, int gameID) {
+        //TODO: Not sure where I code for an observer
+        try {
+            String username = authDatabase.getAuth(authToken);
+            GameData oldGame = gameDatabase.getGame(gameID);
+
+            if (username == null) {
+                throw new RuntimeException("Not a valid authToken");
+            }
+
+            if (Objects.equals(playerColor, "WHITE")) {
+                gameDatabase.updateGame(new GameData(gameID, username, null, oldGame.gameName(),oldGame.game()));
+            } else if (Objects.equals(playerColor, "BLACK")) {
+                gameDatabase.updateGame(new GameData(gameID, null, username, oldGame.gameName(),oldGame.game()));
+            }
+            //Observer case? 
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     private int createGameID() {
         return nextGameID++;
