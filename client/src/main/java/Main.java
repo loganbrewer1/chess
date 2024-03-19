fdb4d991464702ad9dff2df34d83e197910abf5c
Logan Brewer <lbrewer4@byu.edu>
2024-03-18 19:42:46 -0600

    PreLoginHelp function

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -2,12 +2,14 @@ import chess.*;
 
 import java.util.Objects;
 import java.util.Scanner;
+import static ui.EscapeSequences.*;
+
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
-        System.out.printf("Welcome to the Chess App! Type Help to get started. \n>>>");
+        System.out.println(SET_TEXT_COLOR_WHITE + "♕ 240 Chess Client: " + piece);
+        System.out.print("Welcome to the Chess App! Type Help to get started. \n>>>");
         PreLogin();
     }
 
@@ -16,18 +18,23 @@ public class Main {
         while (stillPlaying) {
             Scanner scanner = new Scanner(System.in);
             String line = scanner.nextLine();
-            if (Objects.equals(line, "Help")) {
-                System.out.print("Here is how I can help you\n");
-            } else if (Objects.equals(line, "Register")) {
-                System.out.print("Here are instructions on registering\n");
-            } else if (Objects.equals(line, "Login")) {
-                System.out.print("This is how you login\n");
-            } else if (Objects.equals(line, "Quit")) {
-                stillPlaying = false;
-            } else {
-                System.out.print("Not a valid command. Type Help for a list of commands.\n");
+            switch (line) {
+                case "help" -> PreLoginHelp();
+                case "register" -> System.out.println("Here are instructions on registering");
+                case "login" -> System.out.println("This is how you login");
+                case "quit" -> stillPlaying = false;
+                case null, default -> System.out.println("Not a valid command. Type Help for a list of commands.");
             }
         }
 
+    }
+
+    private static void PreLoginHelp() {
+        String blue = SET_TEXT_COLOR_BLUE;
+        String grey = SET_TEXT_COLOR_LIGHT_GREY;
+        System.out.println(blue + "register <USERNAME> <PASSWORD> <EMAIL>" + grey + " - to create an account");
+        System.out.println(blue + "login <USERNAME> <PASSWORD>" + grey + " - to play chess");
+        System.out.println(blue + "quit" + grey + " - to leave");
+        System.out.print(blue + "help" + grey + " - for a list of helpful commands" + SET_TEXT_COLOR_WHITE + "\n>>>");
     }
 }
