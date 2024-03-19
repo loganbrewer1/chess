3857eb7f88be39e764010112930108f1a8a5f842
Logan Brewer <lbrewer4@byu.edu>
2024-03-18 19:02:23 -0600

    Creating initial prelogin function

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,33 @@
 import chess.*;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+        System.out.printf("Welcome to the Chess App! Type Help to get started. \n>>>");
+        PreLogin();
+    }
+
+    private static void PreLogin() {
+        boolean stillPlaying = true;
+        while (stillPlaying) {
+            Scanner scanner = new Scanner(System.in);
+            String line = scanner.nextLine();
+            if (Objects.equals(line, "Help")) {
+                System.out.print("Here is how I can help you\n");
+            } else if (Objects.equals(line, "Register")) {
+                System.out.print("Here are instructions on registering\n");
+            } else if (Objects.equals(line, "Login")) {
+                System.out.print("This is how you login\n");
+            } else if (Objects.equals(line, "Quit")) {
+                stillPlaying = false;
+            } else {
+                System.out.print("Not a valid command. Type Help for a list of commands.\n");
+            }
+        }
+
     }
 }
